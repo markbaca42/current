84ae230a51f7fb238d5bad0b02bf69bcf2e9cdcc
Mark Baca <markbaca@Marks-MacBook-Pro-4.local>
2022-09-27 20:39:08 -0500

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"; //framwork for creaing webservie applications
 import fetch from "node-fetch";
 
 const app = express();
@@ -9,8 +9,8 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 const redirect_uri = "http://localhost:3000/callback";
-const client_id = "";
-const client_secret = "";
+const client_id = "b5b53e673bf34a2aa429b48736055bae";
+const client_secret = "c851b40319a142408dd5a29140aadc12";
 
 global.access_token;
 
@@ -19,10 +19,10 @@ app.get("/", function (req, res) {
 });
 
 app.get("/authorize", (req, res) => {
-  var auth_query_parameters = new URLSearchParams({
+  var auth_query_parameters = new URLSearchParams({ //send request to get keys for endpoint
     response_type: "code",
     client_id: client_id,
-    scope: "user-library-read",
+    scope: "user-library-read user-top-read",
     redirect_uri: redirect_uri,
   });
 
@@ -31,7 +31,7 @@ app.get("/authorize", (req, res) => {
   );
 });
 
-app.get("/callback", async (req, res) => {
+app.get("/callback", async (req, res) => { //spotify sends message back with code
   const code = req.query.code;
 
   var body = new URLSearchParams({
@@ -41,7 +41,7 @@ app.get("/callback", async (req, res) => {
   });
 
   const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "post",
+    method: "post", //post request
     body: body,
     headers: {
       "Content-type": "application/x-www-form-urlencoded",
@@ -71,27 +71,46 @@ async function getData(endpoint) {
 
 app.get("/dashboard", async (req, res) => {
   const userInfo = await getData("/me");
-  const tracks = await getData("/me/tracks?limit=10");
+  //const tracks = await getData("/me/tracks?limit=10");
+  const topArtist = await getData("/me/top/artists?time_range=long_term&limit=15");
 
-  res.render("dashboard", { user: userInfo, tracks: tracks.items });
+  //res.render("dashboard", { user: userInfo, tracks: tracks.items });
+  res.render("dashboard", { user: userInfo, artists: topArtist.items});
 });
 
-app.get("/recommendations", async (req, res) => {
-  const artist_id = req.query.artist;
-  const track_id = req.query.track;
+// app.get("/recommendations", async (req, res) => {
+//   const artist_id = req.query.artist;
+//   const track_id = req.query.track;
+
+//   const params = new URLSearchParams({
+//     seed_artist: artist_id,
+//     seed_genres: "rock",
+//     seed_tracks: track_id,
+//   });
+
+//   const data = await getData("/recommendations?" + params);
+//   res.render("recommendation", { tracks: data.tracks });
+// });
+
+
+app.get("/top/artists", async (req, res) => {
+  // const artist_id = req.query.artist;
+  // const track_id = req.query.track;
 
   const params = new URLSearchParams({
-    seed_artist: artist_id,
-    seed_genres: "rock",
-    seed_tracks: track_id,
+    type: "artists",
+    time_range: "long_term",
   });
 
   const data = await getData("/recommendations?" + params);
   res.render("recommendation", { tracks: data.tracks });
 });
 
+
+
 let listener = app.listen(3000, function () {
   console.log(
     "Your app is listening on http://localhost:" + listener.address().port
   );
 });
+
