c7695810f7c4b7c03a8585d108c139ec922c1835
Mark Baca <markbaca@Marks-MacBook-Pro-5.local>
2022-10-25 08:46:53 -0500

    all labels have been placed. seeds work.

--- server.js
+++ server.js
@@ -82,15 +82,181 @@ app.get("/makePlaylist", async (req, res) => {
 });
 
 app.post("/makePlaylist", async (req, res) => {
-  console.log(req.body.altrock)
-  const playlist = await getData("/recommendations?limit=10&market=ES&seed_genres=%2Chip-hop%2C" + req.body.altrock);
+  //console.log(req.body.altrock)
+  //const playlist = await getData("/recommendations?limit=10&market=ES&seed_genres=hip-hop" + req.body.altrock);
   
+  const altrock = req.body.altrock;
+  const acoustic = req.body.acoustic;
+  const blues = req.body.blues;
+  const chill = req.body.chill;
+  const country = req.body.country;
+  const dance = req.body.dance;
+  const disco = req.body.disco;
+  const dubstep = req.body.dubstep;
+  const emo = req.body.emo;
+  const funk = req.body.funk;
+  const gospel = req.body.gospel;
+  const hipHop = req.body.hipHop;
+  const jazz = req.body.jazz;
+  const metal = req.body.metal;
+  const opera = req.body.opera;
+  const party = req.body.party;
+  const piano = req.body.piano;
+  const pop = req.body.pop;
+  const punk = req.body.punk;
+  const rnb = req.body.rnb;
+  const reggae = req.body.reggae;
+  const rock = req.body.rock;
+  const romance = req.body.romance;
+  const sad = req.body.sad;
+  const salsa = req.body.salsa;
+  const sleep = req.body.sleep;
+  const soul = req.body.soul;
+  const study = req.body.study;
+  const workOut = req.body.workOut;
 
+  
+  const genre_seeds = [];
+  let seed_count = 0;
+  if(altrock && seed_count < 5){
+    genre_seeds[seed_count] = altrock;
+    seed_count++;
+  }
+  if(acoustic && seed_count < 5) {
+    genre_seeds[seed_count] = acoustic;
+    seed_count++;
+  }
+  if(blues && seed_count < 5) {
+    genre_seeds[seed_count] = blues;
+    seed_count++;
+  }
+  if(chill && seed_count < 5) {
+    genre_seeds[seed_count] = chill;
+    seed_count++;
+  }
+  if(country && seed_count < 5) {
+    genre_seeds[seed_count] = country;
+    seed_count++;
+  }
+  if(dance && seed_count < 5) {
+    genre_seeds[seed_count] = dance;
+    seed_count++;
+  }
+  if(disco && seed_count < 5) {
+    genre_seeds[seed_count] = disco;
+    seed_count++;
+  }
+  if(dubstep && seed_count < 5) {
+    genre_seeds[seed_count] = dubstep;
+    seed_count++;
+  }
+  if(emo && seed_count < 5) {
+    genre_seeds[seed_count] = emo;
+    seed_count++;
+  }
+  if(funk && seed_count < 5) {
+    genre_seeds[seed_count] = funk;
+    seed_count++;
+  }
+  if(gospel && seed_count < 5) {
+    genre_seeds[seed_count] = gospel;
+    seed_count++;
+  }
+  if(hipHop && seed_count < 5) {
+    genre_seeds[seed_count] = hipHop;
+    seed_count++;
+  }
+  if(jazz && seed_count < 5) {
+    genre_seeds[seed_count] = jazz;
+    seed_count++;
+  }
+  if(metal && seed_count < 5) {
+    genre_seeds[seed_count] = metal;
+    seed_count++;
+  }
+  if(opera && seed_count < 5) {
+    genre_seeds[seed_count] = opera;
+    seed_count++;
+  }
+  if(party && seed_count < 5) {
+    genre_seeds[seed_count] = party;
+    seed_count++;
+  }
+  if(piano && seed_count < 5) {
+    genre_seeds[seed_count] = piano;
+    seed_count++;
+  }
+  if(pop && seed_count < 5) {
+    genre_seeds[seed_count] = pop;
+    seed_count++;
+  }
+  if(punk && seed_count < 5) {
+    genre_seeds[seed_count] = punk;
+    seed_count++;
+  }
+  if(rnb && seed_count < 5) {
+    genre_seeds[seed_count] = rnb;
+    seed_count++;
+  }
+  if(reggae && seed_count < 5) {
+    genre_seeds[seed_count] = reggae;
+    seed_count++;
+  }
+  if(rock && seed_count < 5) {
+    genre_seeds[seed_count] = rock;
+    seed_count++;
+  }
+  if(romance && seed_count < 5) {
+    genre_seeds[seed_count] = romance;
+    seed_count++;
+  }
+  if(sad && seed_count < 5) {
+    genre_seeds[seed_count] = sad;
+    seed_count++;
+  }
+  if(salsa && seed_count < 5) {
+    genre_seeds[seed_count] = salsa;
+    seed_count++;
+  }
+  if(sleep && seed_count < 5) {
+    genre_seeds[seed_count] = sleep;
+    seed_count++;
+  }
+  if(soul && seed_count < 5) {
+    genre_seeds[seed_count] = soul;
+    seed_count++;
+  }
+  if(study && seed_count < 5) {
+    genre_seeds[seed_count] = study;
+    seed_count++;
+  }
+  if(workOut && seed_count < 5) {
+    genre_seeds[seed_count] = workOut;
+    seed_count++;
+  }
+
+  console.log(genre_seeds);
+  console.log(req.body.danceability);
+  console.log(req.body.loudness);
+  console.log(req.body.popularity);
+  
+  const params = new URLSearchParams({
+    // seed_artist: "4NHQUGzhtTLFvgF5SZesLK",
+    seed_genres: genre_seeds,
+    target_danceability: req.body.danceability,
+    target_loudness: req.body.loudness,
+    target_popularity: req.body.popularity,
+
+  });
+  
+  const playlist = await getData("/recommendations?" + params); //FIXME: Still not working, i DONT know why it work on spotify site maybe change this line
+  //console.log(playlist);
   res.render("playlist", { songs: playlist.tracks});
 });
 
 app.get("/playlist", async (req, res) => {
   playlist = await getData("/recommendations?limit=10&market=ES&seed_genres=hip-hop");
+
 
   res.render("playlist", { songs: playlist.tracks});
   //how do i get the submit button to move to the next screen.
